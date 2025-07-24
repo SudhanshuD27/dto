2a2754941decf3b3b98ecd46c1fc64c6266400f0
SudhanshuD27 <deshmukhsd2705@gmail.com>
2025-07-24 11:28:48 +0530

    Add files via upload

---
+++ Filters/GlobalExceptionFilter.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EmployeeMVC.Filters
+{
+    public class GlobalExceptionFilter : IExceptionFilter
+    {
+        private readonly IWebHostEnvironment _env;
+
+        public GlobalExceptionFilter(IWebHostEnvironment env)
+        {
+            _env = env;
+        }
+
+        public void OnException(ExceptionContext context)
+        {
+            var exception = context.Exception;
+
+            LogErrorToFile(exception);
+
+            context.Result = new JsonResult(new
+            {
+                success = false,
+                message = exception.Message
+            })
+            {
+                StatusCode = 500
+            };
+
+            context.ExceptionHandled = true;
+        }
+
+        private void LogErrorToFile(Exception exception)
+        {
+            string logPath = Path.Combine(_env.WebRootPath, "logs");
+            if (!Directory.Exists(logPath))
+                Directory.CreateDirectory(logPath);
+
+            string fullPath = Path.Combine(logPath, "error_log.txt");
+            using (StreamWriter writer = new StreamWriter(fullPath, true))
+            {
+                writer.WriteLine($"{DateTime.Now}: {exception.Message}");
+                //writer.WriteLine(exception.Message);
+                writer.WriteLine("---------------------------");
+            }
+        }
+    }
+}
+
+
---
+++ ManagerController.cs
@@ -0,0 +1,54 @@
+﻿using EmployeeMVC.Models;
+using EmployeeMVC.Repository;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EmployeeMVC.Controllers
+{
+    public class ManagerController : Controller
+    {
+        IManagerService mgr;
+
+        public ManagerController(IManagerService mgr)
+        {
+            this.mgr = mgr;
+        }
+
+        public IActionResult Index()
+        {
+            var data = mgr.DisplayManagers();
+            return View(data);
+        }
+
+        public IActionResult AddManager()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult AddManager(Manager m)
+        {
+            mgr.AddManager(m);
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult DeleteManager(int id)
+        {
+            mgr.DeleteManager(id);
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult EditManager(int id)
+        {
+            var data = mgr.FindManagerById(id);
+            return View(data);
+        }
+
+        [HttpPost]
+        public IActionResult EditManager(Manager m)
+        {
+            mgr.UpdateManager(m);
+            return RedirectToAction("Index");
+        }
+    }
+}
+
